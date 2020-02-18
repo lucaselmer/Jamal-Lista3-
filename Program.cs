21d553e7ab07e57170c4e53524dc54b33a2d5f57
LUCAS ELMER MARTINS <lucas.martins2@alunos.sc.senac.br>
2020-02-17 22:03:09 -0300

    Lista 03 17/02/2020

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace Jamal_Lista3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        /*              ** Para escrever Tabuada **
+        try{
+
+            using (StreamWriter escrita = new StreamWriter("save.txt")){
+                int i;
+                int num;
+                Console.WriteLine("Qual a Tabuado desejada");
+                num = Convert.ToInt32(Console.ReadLine());
+                for(i=1; i<=10; i++) {                
+                    escrita.WriteLine("\n" +num+" x "+i+" = "+num*i+"\n");
+                    Console.WriteLine("\n" +num+" x "+i+" = "+num*i);
+                }
+                
+            }
+        } catch {
+
+            }*/
+        try{
+
+            using (StreamReader leitura = new StreamReader("save.txt")){
+                string line;
+                while ((line = leitura.ReadLine()) != null){
+                    Console.WriteLine(line);
+                }
+
+            }
+        } catch(Exception e){
+            Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
